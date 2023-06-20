83258b4dfb4f4b60b725c2bdc8dd87253bbd4686
Tahirah Roohi <130206321+TahirahRoohi271@users.noreply.github.com>
2023-06-20 09:54:31 +0500

    Add files via upload

---
+++ replacing characters/app.js
@@ -0,0 +1,42 @@
+// Write a program that prompts the user to enter a sentence and replaces all occurrences of a specific character with a hyphen (-) symbol. Display the updated sentence.
+
+// var useInput = prompt("Enter Any Sentece");
+// var updated = useInput.replace(/[@#%~`.*+?^${}()|[\]\\]/g, "-");
+// alert(updated);
+
+
+// Write a program that prompts the user to enter a word and replaces all vowels in the word with an exclamation mark (!). Display the updated word.
+
+// var useInput = prompt("Enter Any Sentence");
+// var updated = useInput.replace(/[aeiou]/gi, "!");
+// alert(updated);
+
+
+// Write a program that prompts the user to enter a sentence and replaces all spaces in the sentence with an underscore (_). Display the updated sentence.
+
+// var useInput = prompt("Enter Any Sentence");
+// var updated = useInput.replace(/ /g, "_");
+// alert(updated);
+
+
+// Write a program that prompts the user to enter a sentence and replaces the first occurrence of a specific character with a dollar sign ($). Display the updated sentence.
+
+// var useInput = prompt("Enter Any Sentence");
+// var updated = useInput.slice(0, 1);
+// var newUpdated = "$";
+// var updatedd = newUpdated + useInput.slice(1);
+// alert(updatedd);
+
+
+// Write a program that prompts the user to enter a sentence and replaces the last occurrence of a specific character with an ampersand (&). Display the updated sentence.
+
+// var useInput = prompt("Enter Any Sentence");
+// var updated = useInput.slice(0, -1) + '&';
+// alert(updated);
+
+
+// Write a program that prompts the user to enter a sentence and replaces all lowercase letters with uppercase letters. Display the updated sentence.
+
+// var useInput = prompt("Enter a sentence:");
+// var updated = sentence.toUpperCase();
+// alert(updated);
